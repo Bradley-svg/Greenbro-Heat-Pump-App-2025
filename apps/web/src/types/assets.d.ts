29ed0339788dbe22174a7dc156be373ae00dbc83
Bradley-svg <bradleyayliffl@gmail.com>
2025-10-24 10:07:54 +0200

    committ

---
+++ apps/web/src/types/assets.d.ts
@@ -0,0 +1,4 @@
+declare module '*.css' { const url: string; export default url; }
+declare module '*.svg' { const url: string; export default url; }
+declare module '*.png' { const url: string; export default url; }
+declare module '*.webp' { const url: string; export default url; }
